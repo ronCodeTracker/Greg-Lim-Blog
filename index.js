8668cdc559a204e06dbc1f96fffe71fa2213732e
ronCodeTracker <2fixelectronics777@gmail.com>
2023-11-08 21:44:35 -0600

    path mod

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 const express = require('express')
 
+const path = require('path')
 
 const app = new express()
+
+
 
 app.use(express.static('public'))
 
@@ -13,5 +16,23 @@ app.use(express.static('public'))
 
 app.listen(4000, () => {
     console.log('Listening on port 4000')
+})
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
 })
 
