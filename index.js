12a40479515557c0740866cddecaa50b1b9823f6
ronCodeTracker <2fixelectronics777@gmail.com>
2023-11-09 09:58:44 -0600

    ejs mod

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path')
 
 const app = new express()
 
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
+
 
 
 app.use(express.static('public'))
@@ -21,18 +25,18 @@ app.listen(4000, () => {
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
